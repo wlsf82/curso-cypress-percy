845beb521e23c3a0b3e70e05d392358f90a78d62
Walmyr <wlsf82@gmail.com>
2020-09-28 23:54:08 +0200

    Add three more tests

--- cypress/integration/ticketbox.js
+++ cypress/integration/ticketbox.js
@@ -4,4 +4,25 @@ describe('Ticketbox', () => {
   it('Checks for the initial state', () => {
     cy.percySnapshot();
   });
+
+  it('Checks for invalid email', () => {
+    cy.get('#email').type('johndoe-example.com');
+    cy.percySnapshot();
+  });
+
+  it('Enables submittion after all mandatory fields are filled', () => {
+    cy.get('#first-name').type('John');
+    cy.get('#last-name').type('Doe');
+    cy.get('#email').type('johndoe@example.com');
+    cy.get('#agree').check();
+    cy.percySnapshot();
+  });
+
+  it('Updates agreement based on full name, tickets quantity, and type', () => {
+    cy.get('#first-name').type('John');
+    cy.get('#last-name').type('Doe');
+    cy.get("#ticket-quantity").select("4");
+    cy.get("#vip").check();
+    cy.percySnapshot();
+  });
 });
