3a7595f8845491c2cbe617ffe3a6ff4b2ad81f4a
Walmyr <wlsf82@gmail.com>
2020-09-28 23:58:05 +0200

    Add last test
    Also, refactor the code with some custom commands.
    And make percy snapshot verification able to deal with dynamic
    data.

--- cypress/integration/ticketbox.js
+++ cypress/integration/ticketbox.js
@@ -11,10 +11,7 @@ describe('Ticketbox', () => {
   });
 
   it('Enables submittion after all mandatory fields are filled', () => {
-    cy.get('#first-name').type('John');
-    cy.get('#last-name').type('Doe');
-    cy.get('#email').type('johndoe@example.com');
-    cy.get('#agree').check();
+    cy.fillMandatoryFields()
     cy.percySnapshot();
   });
 
@@ -24,5 +21,13 @@ describe('Ticketbox', () => {
     cy.get("#ticket-quantity").select("4");
     cy.get("#vip").check();
     cy.percySnapshot();
+  });
+
+  const successfulFormSubmission = 'Shows a success message after form submission'
+  it(successfulFormSubmission, () => {
+    cy.fillMandatoryFieldsAndSubmit();
+    cy.percySnapshot(successfulFormSubmission, {
+      percyCSS: `.success span { display: none; }`
+    });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,15 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 import '@percy/cypress';
+
+Cypress.Commands.add('fillMandatoryFields', () => {
+  cy.get('#first-name').type('John');
+  cy.get('#last-name').type('Doe');
+  cy.get('#email').type('john@doe.com');
+  cy.get('#agree').check();
+});
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
+  cy.fillMandatoryFields();
+  cy.contains('Confirm Tickets').click();
+});
